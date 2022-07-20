ce90a38e2a03aca85b7d477657b08d4f63c16fed
Mafiu <mf.milewski@gmail.com>
2022-07-20 19:11:22 -0300

    CameraMovement added + Playground Scene created

---
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private Camera gameCamera;
+    [SerializeField] private float movementSpeed = 5;
+    
+    public void MoveCamera(Vector3 inputVector)
+    {
+        var movementVector = Quaternion.Euler(0,30,0) * inputVector;
+        gameCamera.transform.position += movementVector * Time.deltaTime * movementSpeed;
+    }
+}
